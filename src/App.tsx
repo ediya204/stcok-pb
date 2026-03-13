fc50ecc9feb22a0837df32f284b2510aacf005a5
Edi <edi@Edis-Mac-mini.local>
2026-03-13 16:32:52 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2877,15 +2877,13 @@ export default function App() {
         onLogout={() => setIsLogoutDialogOpen(true)}
       />
       
-      <main className="flex flex-1 overflow-hidden relative pb-14 lg:pb-0">
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <div className="flex-1 overflow-hidden">
-            <AnimatePresence mode="wait">
-              {renderContent()}
-            </AnimatePresence>
-          </div>
-          <SiteFooter />
+      <main className="flex-1 flex flex-col relative pb-14 lg:pb-0 overflow-y-auto">
+        <div className="flex-1">
+          <AnimatePresence mode="wait">
+            {renderContent()}
+          </AnimatePresence>
         </div>
+        <SiteFooter />
       </main>
       
       {/* Toasts */}
